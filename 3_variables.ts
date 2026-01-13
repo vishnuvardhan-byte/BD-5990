6ab7505837b4a6a5181c82e76d50a82b3250acfa
vishnuvardhan-byte <vishnuvardhan@medianv.com>
2026-01-13 18:28:03 +0530

    testing webhook url

---
+++ 3_variables.ts
@@ -0,0 +1,26 @@
+// string
+const name:string="vishnu";
+console.log(name);
+
+// number
+const age=23
+const salary=25.000;
+console.log(age, salary.toFixed(4))
+
+// boolean
+const isStudent:boolean=true;
+console.log(isStudent);
+
+// any
+
+var user_any: any="one";
+var user_any:any=true;
+console.log(user_any);
+
+function sample(name){
+console.log(name)
+}
+
+sample("vishnu");
+
+export{}
